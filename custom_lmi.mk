9b9363e0a737a31cf964d868d71c724bac375a7f
LilFishyChan <xiaomeng200404@gmail.com>
2026-07-15 16:37:25 +0800

    lmi: restore custom_lmi product config

---
+++ custom_lmi.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lmi device
+$(call inherit-product, device/xiaomi/lmi/device.mk)
+
+# Inherit some common PixelOS/custom stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+
+PRODUCT_NAME := custom_lmi
+PRODUCT_DEVICE := lmi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi K30 Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="lmi-user 12 RKQ1.211001.001 V14.0.1.0.SJKMIXM release-keys" \
+    BuildFingerprint=Redmi/lmi/lmi:12/RKQ1.211001.001/V14.0.1.0.SJKMIXM:user/release-keys
