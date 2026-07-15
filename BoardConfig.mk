f1fbb8d63013f84a04c2b71f615972af347ad5c1
LilFishyChan <xiaomeng200404@gmail.com>
2026-07-15 18:05:46 +0800

    lmi: restore kernel config and broken rules in BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,11 +9,18 @@ include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/lmi
 
+# Broken rules
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Display
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/lmi.config
+TARGET_KERNEL_SOURCE := kernel/xiaomi/lmi
+TARGET_KERNEL_VERSION := 4.19
+TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 LLVM_IAS=1
+TARGET_KERNEL_CONFIG := lmi_defconfig
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := lmi
